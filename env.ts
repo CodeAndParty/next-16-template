aa6c6793c0f874bb74a933eb4fe6e780c9e39f20
Alexander Drapkin <alexander.drapkin@outlook.com>
2025-12-14 15:58:33 +0200

    Add the rainwalk api enviroment varibles

--- env.ts
+++ env.ts
@@ -4,9 +4,13 @@ import * as v from "valibot";
 const env = createEnv({
   server: {
     NODE_ENV: v.optional(v.picklist(["development", "production", "test"]), "development"),
+    RAINWALK_API_KEY: v.string(),
+    RAINWALK_API_URL: v.string(),
   },
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
+    RAINWALK_API_KEY: process.env.RAINWALK_API_KEY,
+    RAINWALK_API_URL: process.env.RAINWALK_API_URL,
   },
   emptyStringAsUndefined: true,
 });
